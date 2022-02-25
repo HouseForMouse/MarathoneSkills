45652562c17a5d47f96670070e11845df7ce98d7
AnastasiyaPochkalina <pochkalina01@gmail.com>
2022-02-25 15:58:20 +0300

    Designe of mobilka

---
+++ MarathonSkills/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarathonSkills
+//
+//  Created by Student on 25.02.2022.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var inputMail: UITextField!
+    
+
+    @IBOutlet weak var inputPass: UITextField!
+}
+
